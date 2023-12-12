a692b44bf1b69cc8cc47f1c20a847112eb299573
drycov <drycov@gmail.com>
2023-12-13 01:42:55 +0600

    3000

--- src/app/configs/web.config.ts
+++ src/app/configs/web.config.ts
@@ -1,5 +1,5 @@
 export default {
-    port: 80,
+    port: 3000,
     sesionKey: 'Zero0One1Two2Three3Four4',
     mongoURI: 'mongodb+srv://ttc-ttcnsa:GJWPwBmlUJvc9S2S@cluster0.c48ewsz.mongodb.net/hmapttc',
     defaultLanguage: "en",
